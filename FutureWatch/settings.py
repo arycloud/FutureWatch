245d6887f69217b2dd1a71147f1bb836ce31ef52
Abdul Rehman Yousaf <abdul12391@gmail.com>
2023-06-20 21:17:54 +0500

    added production dependencies and static_root

--- FutureWatch/settings.py
+++ FutureWatch/settings.py
@@ -95,7 +95,7 @@ STATIC_URL = "/assets/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory where uploaded media is saved.
 MEDIA_URL = '/media/' # Public URL at the browser
 
